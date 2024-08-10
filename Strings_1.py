868285b0cab9b29591cffc24ebbc49f83186f437
Muhammad-UmarFarooq <mdumar0508@gmail.com>
2024-08-10 10:04:42 +0530

    Update Strings_1.py

--- Strings_1.py
+++ Strings_1.py
@@ -16,6 +16,9 @@ para="""hi
    hello
 bye"""
 print(para)
+# we can also specify each character in a seperate single or double quotes
+name='r''a''h''u'"l"
+print(name)
 '''When a string contains quotes, 
 using a different type of quote 
 to enclose the string can make it 
@@ -28,3 +31,8 @@ print(test2)
 # when u insert the \ -- the character that just follows the \ is taken as a part of the string without any special meaning
 test3="hello\" \"hi"
 print(test3)
+# In addition to using triple quotes for multi-line strings, you can use the backslash (\) to continue a string onto the next line without including a newline character
+test4="this is a long string "\
+      " that split across multiple lines"\
+" for better readability."
+print(test4)
