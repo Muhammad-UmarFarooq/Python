2f20279d6b0eac7c9c866ebb7dbf407d63fe87e4
Muhammad-UmarFarooq <mdumar0508@gmail.com>
2024-08-12 12:28:54 +0530

    Create Arithmetic_m2.py

---
+++ Operators/Arithmetic_m2.py
@@ -0,0 +1,22 @@
+a=int(input("Enter a value:"))
+b=int(input("Enter b value:"))
+# Addition
+print('Sum: ', a+b)
+
+# Subtraction
+print ('Subtraction: ', a - b)   
+
+# Multiplication
+print ('Multiplication: ', a * b)  
+
+# Division
+print ('Division:', a / b) 
+
+# Floor division
+print('Floor division: ', a//b)
+
+# Modulo
+print('Modulo: ', a%b)
+
+# a to the power b
+print('Power: ', a ** b)
