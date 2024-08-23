a87c5b5cf40a134ca4e29a853e20c3db128747a4
Muhammad-UmarFarooq <mdumar0508@gmail.com>
2024-08-23 15:17:59 +0530

    Update Assignment_m1.py

--- Operators/Assignment_m1.py
+++ Operators/Assignment_m1.py
@@ -15,3 +15,25 @@ b//=2
 print(b)
 b**=2
 print(b)
+# Bitwise and
+a=4
+a&=2
+print(a)
+# Bitwise or
+a|=3
+print(a)
+# Bitwise xor
+a=3
+b=5
+a^=b
+print(a)
+# Bitwise rightshift
+a=3
+b=5
+a>>=b
+print(a)
+# Bitwise leftshift
+a=3
+b=5
+a<<=b
+print(a)
