01f61ecb59236f6d2294fe11e50a7778142f86a4
Muhammad-UmarFarooq <mdumar0508@gmail.com>
2024-08-23 15:11:14 +0530

    Create Bitwise_m1.py

---
+++ Operators/Bitwise_m1.py
@@ -0,0 +1,22 @@
+# Bitwise and
+a=4
+a&=2
+print(a)
+# Bitwise or
+a|=3
+print(a)
+# Bitwise xor
+a=3
+b=5
+a^=b
+print(a)
+# Bitwise rightshift
+a=3
+b=5
+a>>=b
+print(a)
+# Bitwise leftshift
+a=3
+b=5
+a<<=b
+print(a)
