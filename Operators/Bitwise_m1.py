461eee8001a36569e133518ff08769ec1f225bae
Muhammad-UmarFarooq <mdumar0508@gmail.com>
2024-08-23 15:18:25 +0530

    Update Bitwise_m1.py

--- Operators/Bitwise_m1.py
+++ Operators/Bitwise_m1.py
@@ -1,22 +1,20 @@
-# Bitwise and
-a=4
-a&=2
-print(a)
-# Bitwise or
-a|=3
-print(a)
-# Bitwise xor
-a=3
-b=5
-a^=b
-print(a)
-# Bitwise rightshift
-a=3
-b=5
-a>>=b
-print(a)
-# Bitwise leftshift
-a=3
-b=5
-a<<=b
-print(a)
+a=10
+b=4
+print(a & b)
+a=10
+b=4
+print(a|b)
+a=10
+b=4
+print(a^b)
+a=10
+b=4
+print(~a)
+a=10
+b=-10
+print(a>>1)
+print(b>>1)
+a=5
+b=-10
+print(a<<1)
+print(b<<1)
